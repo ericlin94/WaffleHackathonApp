4f4cf0c9408c9c24dc0e0e2a3e0dbd158b5905f0
ericlin94 <eric50818244@gmail.com>
2018-06-20 04:11:09 -0400

    Primary functions finished

--- my-first-app/index.js
+++ my-first-app/index.js
@@ -1,22 +1,83 @@
+var JaroWinkler=require("./JaroWinkler.js")
+var prefixCheck=require("./prefix-check.js")
+var nodemailer = require('nodemailer');
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
-  app.on('push', context => {
-  // Code was just pushed.
-  		app.log('success pushed')
-  });
+  app.on(['pull_request.opened', 'pull_request.edited'], prefixCheck)
   app.on('issues.opened', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello World!'})
+  	/*Authentication*/
+  	const github = await app.auth(context.payload.installation.id);
+  	/*Getting all issues*/
+  	const allIssues = await github.paginate(github.issues.getForRepo(context.repo()), issues => issues);
+  	/*Getting other issues*/
+  	const otherIssues = allIssues[0].data.filter(issue => issue.number !== context.issue().number);
+  	/*The issue opened*/
+  	const theIssue = allIssues[0].data.find(issue => issue.number === context.issue().number);
+  	/*Similar Score*/
+  	var similarScore=0;
+  	/*Similar issue result*/
+  	var result;
+  	/*Find out the issue that has the highest similar score*/
+  	for(var i=0;i<otherIssues.length;i++){
+  		if(JaroWinkler.Distance(theIssue.body,otherIssues[i].body )>similarScore){
+  			result=otherIssues[i];
+  			similarScore=JaroWinkler.Distance(theIssue.body,otherIssues[i].body )
+  		}
+  	}
 
-    // Post a comment on the issue
-    return context.github.issues.createComment(params)
+  	
+  	if(similarScore>0.4&&result!==undefined){
+  		/*Comment Parameter*/
+  		const params = context.issue({body: 'Likely similar to #'+result.number})
+
+  		/*Email Function*/
+  		const user=await github.users.getForUser({username:context.payload.issue.user.login})
+	   	var transporter = nodemailer.createTransport({
+		  service: 'gmail',
+		  auth: {
+		    user: 'issuehunter18@gmail.com',
+		    pass: 'Hunter2018'
+		  }
+		});
+		var mailOptions = {
+		  from: 'issuehunter18@gmail.com',
+		  to: user.data.email,
+		  subject: 'Similar issue to:',
+		  text: 'Duplicate issue'+result.html_url
+		};
+		transporter.sendMail(mailOptions, function(error, info){
+		  if (error) {
+		    console.log(error);
+		  } else {
+		    console.log('Email sent: ' + info.response);
+		  }
+		});
+
+		/*Review Similar Issue*/
+		reviewSimilarIssue(result,context,theIssue);
+
+		/*Create Comment*/
+	   	return context.github.issues.createComment(params)
+  	
+  	}
+  	
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  function reviewSimilarIssue(result,resultContext,theIssue){
+  	app.on('issue_comment.created',async context=>{
+  		if(result.state!="closed"&&context.payload.comment.body=="+1"&&result.number==context.payload.issue.number&&context.payload.issue.user.login==result.user.login){
+  			const { issue, comment, repository } = resultContext.payload
+		    const issueCopy = {
+		      'owner': repository.owner.login,
+		      'repo': repository.name,
+		      'number':issue.number,
+		      'state':'closed'
+		    }
+  			return resultContext.github.issues.createComment(resultContext.issue({body: 'This issue will be closed'}))
+  			+resultContext.github.issues.addLabels(resultContext.issue({labels:["duplicate"]}))
+  			+resultContext.github.issues.edit(issueCopy)
+  		}
+  	});
+  }
 }
---
+++ my-first-app/prefix-check.js
@@ -0,0 +1,63 @@
+module.exports = prefixCheck
+
+async function prefixCheck (context) {
+  const {pull_request: pr} = context.payload
+  const currentStatus = await getCurrentStatus(context)
+  const actions = getPrefixNames()
+  const patt = /#[0-9]+-/i
+  const match = title => {
+    for (var i = 0; i < actions.length; i++) {
+      if (title.indexOf(actions[i]) === 0) {
+        return true
+      }
+    }
+    return false
+  }
+  var newStatus
+  if (match(pr.title)) {
+    newStatus = 'success'
+    var text = patt.exec(pr.head.ref)
+    if (text.index === 0 && !(pr.title.indexOf(text.toString()) >= 0)) {
+      await context.github.pullRequests.update(context.repo({
+        number: pr.number,
+        title: pr.title + ' close ' + text.toString()
+      }))
+    }
+  } else {
+    newStatus = 'error'
+  }
+  const logLabel = `${pr.html_url} "${pr.title}"`
+  if (currentStatus === newStatus) {
+    return console.log(`${logLabel} — ${currentStatus} (unchanged)`)
+  }
+  try {
+    await context.github.repos.createStatus(context.repo({
+      sha: pr.head.sha,
+      state: newStatus,
+      target_url: 'https://github.com/apps/wip',
+      description: match ? 'ready for review' : 'Invalid Title Prefix',
+      context: 'Invalid Title Format'
+    }))
+    console.log(`${logLabel} — ${newStatus}`)
+  } catch (error) {
+    console.log(`${logLabel} — ${error}`)
+  }
+}
+
+function getPrefixNames () {
+  const path = require('./package.json')
+  const names = Object.assign({
+    'prefix': {
+      'types': []
+    }
+  }, path.config)
+  return names['prefix'].types
+}
+
+async function getCurrentStatus (context) {
+  const {data: {statuses}} = await context.github.repos.getCombinedStatusForRef(context.repo({
+    ref: context.payload.pull_request.head.sha
+  }))
+
+  return (statuses.find(status => status.context === 'Invalid Title Format') || {}).state
+}
